e870d7dd7e48addd2e5983e3b2b037f53f574602
Alex P <13531657+ajpeng@users.noreply.github.com>
2024-06-25 18:20:49 -0400

    Added initial endpoints

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,32 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Test message");
 });
 
+
+app.get("/trigger-sync", () => {
+  triggerSync("1");
+});
+
+app.get("/trigger-sync/:id", (req: Request, res: Response) => {
+  const menuID = req.params.id;
+  triggerSync(menuID)
+});
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
+
+function triggerSync(menuId: string) {
+  getMenu(menuId);
+}
+
+function getMenu(menuId: string) {
+  const MENU_URL = `https://bite-test-pos-production.herokuapp.com/locations/${menuId}/menu`;
+  fetch(MENU_URL)
+    .then((res) => res.json())
+    .then((json) => {
+      console.log(json);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
---
+++ src/types.ts
@@ -0,0 +1,63 @@
+// Response Body
+type MenuResponse = {
+  sections: Section[];
+  items: Item[];
+  modGroups: ModGroup[];
+  mods: Mod[];
+  discounts: Discount[];
+  orderTypes: OrderType[];
+}
+  
+type Section = {
+  id: string;
+  name: string;
+  itemIds: string[];
+  magicCopyKey: string;
+  imageUrl: string;
+}
+  
+type Item = {
+  id: string;
+  name: string;
+  price: string;
+  modGroupIds: string;
+  magicCopyKey: string;
+  imageUrl: string;
+}
+  
+type ModGroup = {
+  id: string;
+  name: string;
+  modIds: string[];
+  // a mod group is a collection of items that alter the root (eg pizza) item in some way
+  // (extra sauce, no sauce, etc)
+  // sometimes this must have either a minimum or maximum amount before the item can be added to the cart
+  maxMods?: number;
+  minMods?: number;
+}
+  
+type Mod = {
+  id: string;
+  name: string;
+  modGroupIds: string[];
+  price: number;
+}
+  
+type Discount = {
+  id: string;
+  name: string;
+  // a discount can be either fixed or percentage
+  // if fixed, the amount equaling total cents will be defined
+  amount?: number;
+  // if percentage, a float less than one denoting percentage amount
+  rate?: number;
+  couponCode?: string;
+}
+  
+  // Order types indicate how an order will be fulfilled. For example to go, drive-through etc.
+  // These types can be mapped internally to Bite order types so that
+  // they appear correctly on the POS
+type OrderType = {
+  id: string;
+  name: string;
+}
